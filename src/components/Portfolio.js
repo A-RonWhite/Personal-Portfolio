d9e83647d361de000be6e91313b517fab304024d
Aaron White <aaronwhite1234@hotmail.co.uk>
2019-09-24 14:15:34 +0100

    portfilio section copy updates

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -11,31 +11,30 @@ const Portfolio = () => (
   <div className="portfolio" id="portfolio">
     <SquigglyLine />
     <PortfolioItem
-      websiteImg={domusImg}
-      preHeader="UX/ UI Development"
-      title="Domus Group"
-      copy="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin dictum in tellus eget ultricies. Vestibulum quam ante, consectetur eu ornare eu, lacinia et est. Maecenas gravida lacus ut euismod convallis."
-      link="https://brooklandsinteriors.co.uk/"
-      altText="A screen shot of the domus group homepage with a wave effect along the edge"
+      websiteImg={portfolioImg}
+      preHeader="Development"
+      title="Tabb"
+      copy="Tabb is a CRUD app that allows you to save useful links and set tags to keep them easily organised. Easy on any device, means that whenever or wherever you see a website it is simple to save interesting websites to your own account to view anytime. Built with React on the front-end and Express on the back-end."
+      link="https://floating-fjord-09993.herokuapp.com"
+      altText="A screen shot of the Tabb homepage with a wave effect along the edge"
     />
     <SquigglyLine />
     <PortfolioItemRight
       websiteImg={brooklandsImg}
       preHeader="Design/ Development"
       title="Brooklands Interiors"
-      copy="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin dictum in tellus eget ultricies. Vestibulum quam ante, consectetur eu ornare eu, lacinia et est. Maecenas gravida lacus ut euismod convallis."
+      copy="This is a single page static site created as a temporary page for Brooklands Interiors using bootstrap. They wanted something that was well designed and met their branding with enough information to entice customers into their newly opened showroom. I had to work to a site deadline to provide them with a site before their opening date, the full site is currently being developed in Wordpress."
       link="https://brooklandsinteriors.co.uk/"
       altText="A screen shot of the brooklands interiors homepage with a wave effect along the edge"
     />
     <SquigglyLine />
     <PortfolioItem
-      websiteImg={portfolioImg}
+      websiteImg={domusImg}
       preHeader="Design/ Development"
-      title="Tabb"
-      copy="Shortly after joining Domus they started to undertake the massive job of
-        a complete re-build of their website to bring it into"
+      title="This website"
+      copy="This portfolio site is built in React, using Sass and was a fantastic opportunity to explore design without the restrictions that come with client briefs. I wanted to push my design limitations with this project instead of conforming to a layout I could easily/ quickly produce. I also took the time to familiarise myself with using Sass through-out the project."
       link="https://brooklandsinteriors.co.uk/"
-      altText="A screen shot of the Tabb homepage with a wave effect along the edge"
+      altText="A screen shot of my portfolio homepage with a wave effect along the edge"
     />
     <SquigglyLine />
     <div className="other-sites">
